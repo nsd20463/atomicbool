eb869406ea66fbc412b6fa45f8b7ea5fd1a477b3
Nicolas S. Dade <nic.dade@gmail.com>
2017-02-06 13:24:05 -0800

    atomic bools. storing and loading with memory barriers
    so I can set a global bool to communicate that something is done
    and not have to use an int32 for it.
    
    Note the names of the functions are redundant, but they match the names
    in the standard lib sync/atomic.

---
+++ atomicbool.go
@@ -0,0 +1,46 @@
+package atomicbool
+
+import (
+	"sync/atomic"
+	"unsafe"
+)
+
+func StoreBool(addr *bool, val bool) {
+	// figure out what uint32 this bool is part of, and edit the uint32
+	// NOTE WELL we have to do this in such a way that gc moving the bool around will update our local vars,
+	// which in turn means we can't store anything in a uintptr type except in the middle of an expression.
+	// We're also going to make the (currently safe) assumption that the alignment of the bool within the uint32
+	// will not change if gc moves the bool.
+	p32 := (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(addr)) &^ uintptr(3)))
+	shift := 8 * (uintptr(unsafe.Pointer(addr)) - uintptr(unsafe.Pointer(addr)))
+	bits := 8 * unsafe.Sizeof(*addr)
+	mask := (uint32(1) << bits) - 1
+
+	for {
+		i := atomic.LoadUint32(p32)
+		n := i &^ (mask << shift)
+		if val {
+			n |= 1 << shift
+		}
+		if atomic.CompareAndSwapUint32(p32, i, n) {
+			return
+		}
+	}
+}
+
+func LoadBool(addr *bool) (val bool) {
+	// see comments in StoreBool
+	p32 := (*uint32)(unsafe.Pointer(uintptr(unsafe.Pointer(addr)) &^ uintptr(3)))
+	shift := 8 * (uintptr(unsafe.Pointer(addr)) - uintptr(unsafe.Pointer(addr)))
+	bits := 8 * unsafe.Sizeof(*addr)
+	mask := (uint32(1) << bits) - 1
+
+	i := atomic.LoadUint32(p32)
+	i >>= shift
+	i &= mask
+	if i != 0 {
+		val = true
+	}
+
+	return val
+}
